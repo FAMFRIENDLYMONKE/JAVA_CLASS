383d445e9bae5d4236eb12342aa543a817884312
FAMFRIENDLYMONKE <84069775+FAMFRIENDLYMONKE@users.noreply.github.com>
2024-01-21 01:48:30 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -8,16 +8,17 @@ class Test extends Frame{
     public Test(){
         this.setVisible(true);
         this.setSize(500,500);
+        this.setBackground(Color.yellow);
     }
     @Override
     public void paint(Graphics g){
-        
+        g.setColor(Color.red);
         try{Thread.sleep(2000);}
         catch(InterruptedException e){}
         
         int i = 50, j = 10;
         while(i < 500 && j< 250){
-            g.drawOval(j, i,50,50);
+            g.fillOval(j, i,50,50);
             i += 45;
             j += 25;
             try{Thread.sleep(100);}
@@ -27,9 +28,9 @@ class Test extends Frame{
 
         i = 500; j = 250;
         while(i > 300 && j < 400){
-            g.drawOval(j, i,50,50);
-            i -= 40;
-            j += 30;
+            g.fillOval(j, i,50,50);
+            i -= 20;
+            j += 15;
             try{Thread.sleep(100);}
             catch(InterruptedException e){}
             g.clearRect(0,0,500,500);
@@ -37,7 +38,7 @@ class Test extends Frame{
 
         i = 300; j = 400;
         while(i < 500 && j < 450){
-            g.drawOval(j, i,50,50);
+            g.fillOval(j, i,50,50);
             i += 20;
             j += 5;
             try{Thread.sleep(100);}
@@ -45,6 +46,6 @@ class Test extends Frame{
             g.clearRect(0,0,500,500);
         }
 
-        g.drawOval(500, 450, 5, 5);
+        g.fillOval(500, 450, 5, 5);
     }
 }
